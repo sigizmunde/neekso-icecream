ae8c4dfbe7f3fb7dd08786f7c7983a98da3d6d7d
sigizmunde <91430675+sigizmunde@users.noreply.github.com>
2022-01-14 21:10:26 +0000

    Deploying to gh-pages from @ sigizmunde/neekso-icecream@17d4e1268cd48651ab7d9a0ece27e169ab6e6173 🚀

--- src.da9dce44.js
+++ src.da9dce44.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/neekso-icecream/src.100c83b5.js.map
+//# sourceMappingURL=/neekso-icecream/src.da9dce44.js.map
