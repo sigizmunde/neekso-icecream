4fbfcb90af68aa1ce8266d3e08b6a14a43a97220
illya bondar <1816579@gmail.com>
2022-01-16 21:15:50 +0200

    about js fixed

--- src/js/about.js
+++ src/js/about.js
@@ -1,12 +1,13 @@
 let dots = document.getElementById("about-dots");
 let more = document.getElementById("about-more");
 let btn = document.getElementById("about-btn");
+let btnInnerHtmlCashe = btn.innerHTML;
 
 function readMore() {
 
             if (dots.style.display === "none") {
                 dots.style.display = "inline";
-                btn.innerHTML="Read more"
+                btn.innerHTML = btnInnerHtmlCashe;
                 more.style.display="none";
             } else {
                 dots.style.display = "none";
